e072687189cea6f95af6ee6af57677496747909c
m-luebbers <matthew.luebbers@colorado.edu>
2020-10-03 00:47:43 -0600

    NewMenuClicker now up to featureset of MenuClicker
    Now all that remains is to add in the functionality specific to constraint parametrization

--- Assets/NewMenuClicker.cs
+++ Assets/NewMenuClicker.cs
@@ -8,6 +8,11 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
     private static int constraintToPass = -1;
     private static Dictionary<int, List<int>> appliedConstraints = new Dictionary<int, List<int>>();
 
+    // For testing constraint application packets
+    //private static Dictionary<int,List<int>> appliedConstraints = new Dictionary<int, List<int>> { { 1, new List<int>() { } }, { 3, new List<int>() { } }, { 5, new List<int>() { } },
+    //{ 7, new List<int>() { } }, { 9, new List<int>() { } }, { 11, new List<int>() { } }, { 12, new List<int>() { } }, { 13, new List<int>() { } }, { 26, new List<int>() { } },
+    //{ 29, new List<int>() { } }, { 32, new List<int>() { } }, { 39, new List<int>() {13,14,15 } }};
+
     // Use this for initialization
     void Start() {
 
@@ -58,7 +63,11 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
             }
             else if (thisObj.name == "SendButton")
             {
-                print("Send");
+                string jsonified = jsonify(appliedConstraints);
+                GameObject scriptHolder = GameObject.FindWithTag("PrimaryScriptHolder");
+                ROSBridgeLib.ROSConnector rosHandler = scriptHolder.GetComponent<ROSBridgeLib.ROSConnector>();
+                rosHandler.PublishThis(jsonified);
+                menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "Constraints Sent";
             }
         }
 
@@ -74,6 +83,7 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
         {
             if (thisObj.name == "BackButton")
             {
+                menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "ARC-LfD v1.0";
                 menu2.SetActive(false);
                 menu1.SetActive(true);
             }
@@ -134,7 +144,7 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
             }
             else if (thisObj.name == "ConfirmButton")
             {
-                print("Confirm");
+                menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "ARC-LfD v1.0";
                 menu3A.SetActive(false);
                 menu1.SetActive(true);
             }
@@ -156,7 +166,7 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
             }
             else if (thisObj.name == "ConfirmButton")
             {
-                print("Confirm");
+                menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "ARC-LfD v1.0";
                 menu3B.SetActive(false);
                 menu1.SetActive(true);
             }
@@ -178,7 +188,7 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
             }
             else if (thisObj.name == "ConfirmButton")
             {
-                print("Confirm");
+                menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "ARC-LfD v1.0";
                 menu3C.SetActive(false);
                 menu1.SetActive(true);
             }
@@ -196,6 +206,7 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
         {
             if (thisObj.name == "BackButton")
             {
+                menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "ARC-LfD v1.0";
                 menu4.SetActive(false);
                 menu1.SetActive(true);
             }
@@ -246,7 +257,7 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
         //MENU 5 "Apply <Constraint Name>"
         //  Go Back -> GOTO Menu 4
         //  Add Constraint to Keyframes -> GOTO Menu 6
-        //  Remove from Trajectory -> update trajectory representation; GOTO Menu 1
+        //  Remove from Trajectory -> update trajectory representation; display confirmation message
         else if (menu5.activeInHierarchy)
         {
             if (thisObj.name == "BackButton")
@@ -256,13 +267,66 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
             }
             else if (thisObj.name == "ApplyButton")
             {
+                UnityEngine.GameObject[] objs = GameObject.FindGameObjectsWithTag("Respawn");
+                int max_kfid = 0;
+                foreach (UnityEngine.GameObject ball in objs)
+                {
+                    if (ball.GetComponent<StatePrefab>().order > max_kfid)
+                    {
+                        max_kfid = ball.GetComponent<StatePrefab>().order;
+                    }
+                }
+                foreach (UnityEngine.GameObject ball in objs)
+                {
+                    ball.tag = "StartConstraint";
+                    int kfid = ball.GetComponent<StatePrefab>().order;
+                    Clicker clicker = ball.GetComponent<Clicker>();
+                    bool constraint_violated = clicker.CheckConstraints(ball.GetComponent<StatePrefab>().constraints);
+                    // Color all keyframes in violation red
+                    if (constraint_violated)
+                    {
+                        ball.GetComponent<MeshRenderer>().material.color = Color.red;
+                        ball.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.red;
+                        ball.transform.GetChild(0).transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.red;
+                        ball.transform.GetChild(0).transform.GetChild(2).GetComponent<MeshRenderer>().material.color = Color.red;
+                        ball.transform.GetChild(0).transform.GetChild(3).GetComponent<MeshRenderer>().material.color = Color.red;
+                    }
+                    // Color all other keyframes according to a white -> green gradient
+                    else
+                    {
+                        float factor = 255.0f / max_kfid;
+                        float kcolor = (factor * kfid) / 255.0f;
+                        if (kcolor > 1.0f)
+                        {
+                            kcolor = 1.0f;
+                        }
+                        ball.GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                        ball.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                        ball.transform.GetChild(0).transform.GetChild(1).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                        ball.transform.GetChild(0).transform.GetChild(2).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                        ball.transform.GetChild(0).transform.GetChild(3).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                    }
+                }
+                UnityEngine.GameObject[] cons = GameObject.FindGameObjectsWithTag("GameController");
+                foreach (UnityEngine.GameObject constraint in cons)
+                {
+                    constraint.transform.position = new Vector3(-999, -999, 0);
+                }
+                menu6.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = "Select Starting Keyframe";
                 menu5.SetActive(false);
                 menu6.SetActive(true);
             }
             else if (thisObj.name == "ClearButton")
             {
-                print("Clear");
-                print(constraintToPass);
+                foreach (List<int> value in appliedConstraints.Values)
+                {
+                    if (value.Contains(constraintToPass))
+                    {
+                        value.Remove(constraintToPass);
+                    }
+                }
+                revisualizeClear(constraintToPass);
+                menu5.transform.GetChild(0).gameObject.GetComponent<TextMesh>().text = "Constraint Cleared from Model";
             }
         }
 
@@ -275,13 +339,254 @@ public class NewMenuClicker : MonoBehaviour, IInputClickHandler {
         {
             if (thisObj.name == "BackButton")
             {
+                UnityEngine.GameObject[] start_objs = GameObject.FindGameObjectsWithTag("StartConstraint");
+                foreach (UnityEngine.GameObject ball in start_objs)
+                {
+                    ball.tag = "Respawn";
+                }
+                UnityEngine.GameObject[] end_objs = GameObject.FindGameObjectsWithTag("EndConstraint");
+                foreach (UnityEngine.GameObject ball in end_objs)
+                {
+                    ball.tag = "Respawn";
+                }
                 menu6.SetActive(false);
                 menu5.SetActive(true);
+
+                // Reset constraint application
+                GlobalHolder.endID = -1;
+                GlobalHolder.startID = -1;
             }
             else if (thisObj.name == "ConfirmButton")
             {
-                print("Confirm");
+                // Package constraint data structure here to send when the user hits "finished"
+
+                // DEBUG PRINTS
+                //print(constraintToPass);
+                //print(GlobalHolder.startID);
+                //print(GlobalHolder.endID);
+
+                // Validating that the constraint makes sense
+                if (constraintToPass != -1 && GlobalHolder.endID >= GlobalHolder.startID && GlobalHolder.startID >= 0)
+                {
+                    // Look thru dictionary to add the applied constraint over the range
+                    foreach (int kfid in appliedConstraints.Keys)
+                    {
+                        if (kfid >= GlobalHolder.startID && kfid <= GlobalHolder.endID)
+                        {
+                            if (!appliedConstraints[kfid].Contains(constraintToPass))
+                            {
+                                appliedConstraints[kfid].Add(constraintToPass);
+                            }
+                        }
+                    }
+                    //DEBUG PRINT
+                    //print(appliedConstraints);
+
+                    UnityEngine.GameObject[] start_objs = GameObject.FindGameObjectsWithTag("StartConstraint");
+                    foreach (UnityEngine.GameObject ball in start_objs)
+                    {
+                        ball.tag = "Respawn";
+                    }
+                    UnityEngine.GameObject[] end_objs = GameObject.FindGameObjectsWithTag("EndConstraint");
+                    foreach (UnityEngine.GameObject ball in end_objs)
+                    {
+                        ball.tag = "Respawn";
+                    }
+                    menu1.transform.GetChild(2).gameObject.GetComponent<TextMesh>().text = "ARC-LfD v1.0";
+                    menu6.SetActive(false);
+                    menu1.SetActive(true);
+                    revisualize();
+
+                    // Reset constraint application
+                    constraintToPass = -1;
+                    GlobalHolder.endID = -1;
+                    GlobalHolder.startID = -1;
+                }
+
+                // Validation pass failed - reset everything (except constraintToPass) and bump the user back a menu
+                else
+                {
+                    UnityEngine.GameObject[] start_objs = GameObject.FindGameObjectsWithTag("StartConstraint");
+                    foreach (UnityEngine.GameObject ball in start_objs)
+                    {
+                        ball.tag = "Respawn";
+                    }
+                    UnityEngine.GameObject[] end_objs = GameObject.FindGameObjectsWithTag("EndConstraint");
+                    foreach (UnityEngine.GameObject ball in end_objs)
+                    {
+                        ball.tag = "Respawn";
+                    }
+                    GlobalHolder.endID = -1;
+                    GlobalHolder.startID = -1;
+
+                    menu6.SetActive(false);
+                    menu5.SetActive(true);
+                    menu5.transform.GetChild(0).GetComponent<TextMesh>().text = "Invalid constraint application. Try again.";
+                }
             }
         }
+    }
+
+    // Clear a constraint from the entire trajectory
+    void revisualizeClear(int constraintID)
+    {
+        Dictionary<string, DynamicPoints.TrajectoryPoint> pointsDict = DynamicPoints.DynamicTrajectoryReader.pointsDict;
+        GameObject scriptHolder = GameObject.FindWithTag("PrimaryScriptHolder");
+        DynamicPoints.DynamicTrajectoryReader dtr = scriptHolder.GetComponent<DynamicPoints.DynamicTrajectoryReader>();
+        print(pointsDict);
+        List<string> pointkeyList = new List<string>();
+        // Use a separate list to prevent out-of-sync errors
+        foreach (string pointkey in pointsDict.Keys)
+        {
+            pointkeyList.Add(pointkey);
+        }
+        // Loop over that list
+        foreach (string pointkey in pointkeyList)
+        {
+            DynamicPoints.TrajectoryPoint point = pointsDict[pointkey];
+            int[] constraints = point.applied_constraints;
+            foreach (int constraint in constraints)
+            {
+                // If a keyframe has the constraint in question, remove it from its constraint array
+                if (constraintID == constraint)
+                {
+                    int[] new_constraints = new int[constraints.Length - 1];
+                    int count = 0;
+                    for (int i = 0; i < constraints.Length; i++)
+                    {
+                        if (constraints[i] != constraintID)
+                        {
+                            new_constraints[count] = constraints[i];
+                            count++;
+                        }
+                    }
+                    // Reset constraint array and re-draw
+                    DynamicPoints.DynamicTrajectoryReader.pointsDict[pointkey].applied_constraints = new_constraints;
+                    dtr.DrawTrajectoryPointNoPoint(DynamicPoints.DynamicTrajectoryReader.pointsDict[pointkey]);
+                }
+            }
+        }
+        recolor();
+    }
+
+    // Update internals of DynamicTrajectoryReader so the user sees what they should see once they've added constraint applications
+    // In essence, this is the inverse of the process used to update the MenuClicker's internal dictionary
+    void revisualize()
+    {
+        Dictionary<string, DynamicPoints.TrajectoryPoint> pointsDict = DynamicPoints.DynamicTrajectoryReader.pointsDict;
+        GameObject scriptHolder = GameObject.FindWithTag("PrimaryScriptHolder");
+        DynamicPoints.DynamicTrajectoryReader dtr = scriptHolder.GetComponent<DynamicPoints.DynamicTrajectoryReader>();
+        List<string> pointkeyList = new List<string>();
+        // Use a separate list to prevent out-of-sync errors
+        foreach (string pointkey in pointsDict.Keys)
+        {
+            pointkeyList.Add(pointkey);
+        }
+        // Loop over that list
+        foreach (string pointkey in pointkeyList)
+        {
+            DynamicPoints.TrajectoryPoint point = pointsDict[pointkey];
+            bool is_new = true;
+            int kfid = point.keyframe_id;
+            int[] constraints = point.applied_constraints;
+            foreach (int new_constraint in appliedConstraints[kfid])
+            {
+                is_new = true;
+                foreach (int old_constraint in constraints)
+                {
+                    if (old_constraint == new_constraint)
+                    {
+                        is_new = false;
+                    }
+                }
+                // If there's a constraint that should be applied to keyframe but isn't yet
+                if (is_new)
+                {
+                    int[] new_constraints = new int[constraints.Length + 1];
+                    for (int i = 0; i < constraints.Length; i++)
+                    {
+                        new_constraints[i] = constraints[i];
+                    }
+                    // Add new constraint
+                    new_constraints[constraints.Length] = new_constraint;
+                    // Reset constraint array and re-draw
+                    DynamicPoints.DynamicTrajectoryReader.pointsDict[pointkey].applied_constraints = new_constraints;
+                    dtr.DrawTrajectoryPointNoPoint(DynamicPoints.DynamicTrajectoryReader.pointsDict[pointkey]);
+                }
+            }
+        }
+        recolor();
+    }
+
+    // Make sure the color gradient is preserved after revisualizations
+    void recolor()
+    {
+        UnityEngine.GameObject[] objs = GameObject.FindGameObjectsWithTag("Respawn");
+        int max_kfid = 0;
+        foreach (UnityEngine.GameObject ball in objs)
+        {
+            if (ball.GetComponent<StatePrefab>().order > max_kfid)
+            {
+                max_kfid = ball.GetComponent<StatePrefab>().order;
+            }
+        }
+        foreach (UnityEngine.GameObject ball in objs)
+        {
+            int kfid = ball.GetComponent<StatePrefab>().order;
+            Clicker clicker = ball.GetComponent<Clicker>();
+            bool constraint_violated = clicker.CheckConstraints(ball.GetComponent<StatePrefab>().constraints);
+            if (constraint_violated)
+            {
+                ball.GetComponent<MeshRenderer>().material.color = Color.red;
+                ball.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = Color.red;
+                ball.transform.GetChild(0).transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.red;
+                ball.transform.GetChild(0).transform.GetChild(2).GetComponent<MeshRenderer>().material.color = Color.red;
+                ball.transform.GetChild(0).transform.GetChild(3).GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+            else
+            {
+                float factor = 255.0f / max_kfid;
+                float kcolor = (factor * kfid) / 255.0f;
+                if (kcolor > 1.0f)
+                {
+                    kcolor = 1.0f;
+                }
+                ball.GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                ball.transform.GetChild(0).transform.GetChild(0).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                ball.transform.GetChild(0).transform.GetChild(1).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                ball.transform.GetChild(0).transform.GetChild(2).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+                ball.transform.GetChild(0).transform.GetChild(3).GetComponent<MeshRenderer>().material.color = new Color(kcolor, 1.0f, kcolor);
+            }
+        }
+    }
+
+    // Turn our dictionary into a JSON string to pass over ROS network
+    string jsonify(Dictionary<int, List<int>> dict)
+    {
+        string json_string = "{";
+        foreach (int key in dict.Keys)
+        {
+            json_string += "\\\"" + key.ToString() + "\\\"";
+            json_string += ":{\\\"applied_constraints\\\":[";
+            foreach (int value in dict[key])
+            {
+                json_string += value.ToString();
+                json_string += ",";
+            }
+            // Remove final comma
+            if (json_string[json_string.Length - 1] == ',')
+            {
+                json_string = json_string.Substring(0, json_string.Length - 1);
+            }
+            json_string += "]},";
+        }
+        // Remove final comma
+        if (json_string[json_string.Length - 1] == ',')
+        {
+            json_string = json_string.Substring(0, json_string.Length - 1);
+        }
+        json_string += "}";
+        //print(json_string);
+        return json_string;
     }
 }
